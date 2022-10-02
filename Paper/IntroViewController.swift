91dd951ab9833060bc290797074d90c9077ea3e6
minaje0917 <k050918@naver.com>
2022-10-02 14:34:53 +0900

    :tada: Glog-p

---
+++ Paper/IntroViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Paper
+//
+//  Created by 선민재 on 2022/08/30.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+    }
+
+
+}
+
+extension UIColor {
+    class var background: UIColor?{return UIColor(named: "background")}
+}
---
+++ Paper/IntroViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  IntroViewModel.swift
+//  Paper
+//
+//  Created by 선민재 on 2022/10/02.
+//
+
+import Foundation
