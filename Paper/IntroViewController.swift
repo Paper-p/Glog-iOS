344d71c3bc2f2c8528ae9e519de757ee47028dfe
minaje0917 <k050918@naver.com>
2022-10-03 23:24:34 +0900

    :sparkles: add backgroundView

--- Paper/IntroViewController.swift
+++ Paper/IntroViewController.swift
@@ -15,7 +15,7 @@ class IntroViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .background
+        view.backgroundColor = .white
         addView()
         setLayout()
     }
@@ -28,15 +28,27 @@ class IntroViewController: UIViewController {
         $0.font = UIFont(name: "Pretendard-Bold", size: 18)
     }
     
+    let backgroundView = UIView().then {
+        $0.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.52)
+        $0.layer.cornerRadius = 112
+    }
+    
     private func addView() {
-        [underText].forEach {
+        [backgroundView,underText].forEach {
             view.addSubview($0)
         }
     }
     
     private func setLayout() {
+        backgroundView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.snp.top).offset((bounds.height) / 3.8)
+            $0.height.equalTo((bounds.height) / 2.89)
+            $0.width.equalTo((bounds.width) / 1.33)
+        }
         underText.snp.makeConstraints{
-            $0.center.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(backgroundView.snp.bottom).inset((bounds.height) / 12.78)
         }
     }
     
