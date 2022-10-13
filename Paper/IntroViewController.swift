c284f7d3329b9ffa209ae12fd8fb8284128d72b1
minaje0917 <k050918@naver.com>
2022-10-13 12:03:44 +0900

    :sparkles: add CAGradientLayer

--- Paper/IntroViewController.swift
+++ Paper/IntroViewController.swift
@@ -16,10 +16,20 @@ class IntroViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        let gradient = CAGradientLayer()
+        gradient.frame = self.loginButton.bounds
+//        let colors: [CGColor] = [
+//           .init(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1),
+//           .init(red: 0.5563425422, green: 0.9793455005, blue: 0, alpha: 1),
+//           .init(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+//        ]
+        gradient.colors = [UIColor.Gradient1!.cgColor, UIColor.Gradient2!.cgColor]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        loginButton.layer.addSublayer(gradient)
         addView()
         setLayout()
     }
-    
     
     let underText = UILabel().then {
         $0.text = "여기서 너의 생각을 \n 남들에게 이야기해봐"
@@ -29,6 +39,7 @@ class IntroViewController: UIViewController {
         $0.font = UIFont(name: "Pretendard-Bold", size: 18)
     }
     
+    
     let backgroundView = UIView().then {
         $0.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.52)
         $0.layer.cornerRadius = 112
@@ -37,17 +48,12 @@ class IntroViewController: UIViewController {
         let text = NSAttributedString(string: "로그인")
         $0.setAttributedTitle(text, for: .normal)
         $0.titleLabel?.font = UIFont(name: "Pretendard-ExtraBold", size: 18)
-        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.setTitleColor(UIColor.blue, for: .normal)
         $0.layer.cornerRadius = 10
     }
     
-    let gradient = CAGradientLayer()
-    gradient.frame = loginButton.bounds
-    gradient.colors = [UIColor.orange.cgColor, UIColor.red.cgColor]
-    loginButton.layer.addSublayer(gradient)
-    
     private func addView() {
-        [backgroundView,underText].forEach {
+        [backgroundView,underText,loginButton].forEach {
             view.addSubview($0)
         }
     }
@@ -64,10 +70,18 @@ class IntroViewController: UIViewController {
             $0.centerX.equalToSuperview()
             $0.bottom.equalTo(backgroundView.snp.bottom).inset((bounds.height) / 12.78)
         }
+        loginButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.snp.top).offset(600)
+            $0.height.equalTo(200)
+            $0.width.equalTo(600)
+        }
     }
     
 }
 
 extension UIColor {
     class var background: UIColor?{return UIColor(named: "background")}
+    class var Gradient1: UIColor?{return UIColor(named: "Gradient1")}
+    class var Gradient2: UIColor?{return UIColor(named: "Gradient2")}
 }
