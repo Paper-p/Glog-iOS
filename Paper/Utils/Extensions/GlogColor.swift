75d1116ffa3dbd5d44c2edc1fe2cfe17afeae59e
minaje0917 <k050918@naver.com>
2022-11-07 16:59:33 +0900

    :recycle: Glog Color, PageUI Extension

---
+++ Paper/Utils/Extensions/GlogColor.swift
@@ -0,0 +1,16 @@
+//
+//  GlogColor.swift
+//  Paper
+//
+//  Created by 선민재 on 2022/11/07.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundColor = UIColor(named: "backgroundColor")
+    static var Gradient1 = UIColor(named: "Gradient1")
+    static var Gradient2 = UIColor(named: "Gradient2")
+    static var textField = UIColor(named: "textField")
+    static var icon = UIColor(named: "icon")
+}
---
+++ Paper/Utils/Extensions/PageUI.swift
@@ -0,0 +1,21 @@
+//
+//  PageUI.swift
+//  Paper
+//
+//  Created by 선민재 on 2022/11/07.
+//
+
+import UIKit
+
+extension UITextField {
+    func addLeftImage(_ image: UIImage, x: Float, y: Float) {
+        let iconView = UIImageView(frame:CGRect(x: CGFloat(x), y: CGFloat(y), width: image.size.width, height: image.size.height))
+        iconView.image = image
+        iconView.tintColor = .icon
+        let iconContainerView: UIView = UIView(frame:CGRect(x: 0, y: 0, width: 38, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+
+}
