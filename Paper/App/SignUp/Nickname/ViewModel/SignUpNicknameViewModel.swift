889c4ea80d2403c0add34fdf87c63876039f4fea
minaje0917 <k050918@naver.com>
2022-11-07 20:40:51 +0900

    :sparkles: add SignUpPw, SignUpNickname

---
+++ Paper/App/SignUp/Nickname/ViewModel/SignUpNicknameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpNicknameViewModel.swift
+//  Paper
+//
+//  Created by 선민재 on 2022/11/07.
+//
+
+import Foundation
---
+++ Paper/App/SignUp/Pw/ViewModel/SignUpPWViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpPWViewModel.swift
+//  Paper
+//
+//  Created by 선민재 on 2022/11/07.
+//
+
+import Foundation
