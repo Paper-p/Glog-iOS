70e90b09f37ad9e2352990f8d3c6edb10a2cdd35
minaje0917 <k050918@naver.com>
2022-11-07 15:10:34 +0900

    :sparkles: add idTextField, nextButton

--- Paper/App/SignUp/View/SignUpViewController.swift
+++ Paper/App/SignUp/View/SignUpViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
+import Then
 
 class SignUpViewController: BaseViewController<SignUpViewModel> {
     
@@ -15,5 +17,46 @@ class SignUpViewController: BaseViewController<SignUpViewModel> {
         super.viewDidLoad()
         self.navigationItem.logoImage()
     }
+    
+    override func viewDidLayoutSubviews() {
+        nextButton.glogGradient()
+    }
 
+    lazy var idTextField = UITextField().then {
+        $0.backgroundColor = .textField
+        $0.attributedPlaceholder = NSAttributedString(string: "사용하실 아이디를 입력해주세요", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 1, green: 1, blue: 1, alpha: 0.48)])
+        $0.textColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.48)
+        $0.font = UIFont(name: "Pretendard-Medium", size: 16)
+        $0.addLeftImage(UIImage(systemName: "person.fill")!, x: 13, y:7)
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var nextButton = UIButton().then {
+        let text = NSAttributedString(string: "다음")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Pretendard-ExtraBold", size: 18)
+        $0.setTitleColor(UIColor.black, for: .normal)
+    }
+    
+    override func addView() {
+        [idTextField,nextButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setLayout() {
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top).offset((bounds.height) / 2.411)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo((bounds.height) / 16.23)
+            $0.trailing.leading.equalToSuperview().inset(12)
+        }
+        nextButton.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(60)
+            $0.leading.trailing.equalToSuperview().inset(12)
+        }
+    }
+    
 }
